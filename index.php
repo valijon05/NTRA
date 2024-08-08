d57eb490c67d275d6810b62bd98f32061ef514c4
valijon <valijoneshmamatov98@gmail.com>
2024-08-08 23:42:12 +0500

    HW

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
-declare(strict_type=1);
-
+require_once __DIR__ . '/vendor/autoload.php';
 require 'bootstrap.php';
+
+use App\User;
+
+$user = new User();
+// $user->create(2,"Azizbek","admin","male","+998910058110");
+// $user->updateUser(1,"Valijon","admin","male","+998910058110");
+// $user->deleteUser(1);
+$user->getUser(2);
